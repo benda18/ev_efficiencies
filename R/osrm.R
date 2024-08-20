25052cea84512808340d68db749db6a3d63b4f4a
Tim Bender <bendertj@gmail.com>
2024-08-20 10:30:14 -0400

    censusxy renv

--- R/osrm.R
+++ R/osrm.R
@@ -1,8 +1,16 @@
 library(renv)
 library(dplyr)
-library(osrm)
+library(osrm)  # open source routing machine
+library(remotes)
+library(censusxy)
+
+# install.packages("remotes")
+# remotes::install_github("chris-prener/censusxy")
 
 renv::status()
 renv::snapshot()
 
 rm(list=ls())
+
+
+?osrm::osrmRoute()
